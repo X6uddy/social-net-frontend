94a3a5630e5f2f6319c868e3119403e2d8ecd391
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-19 21:01:18 +0400

    layout created

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+
+import LayoutClient from "@/components/layout/Layout";
+
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Social-net Xbuddy",
+  description: "My first social-net",
+  icons: '/logo.svg',
+  // для SEO нужно указать openGraph, twitter, robots, verification
+};
+
+export const viewport: Viewport = {
+  themeColor: '#0E0B18',
+  colorScheme: 'dark'
+} // новая фишка некста, теперь viewport указываается отдельно
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <LayoutClient>{children}</LayoutClient>
+      </body>
+    </html>
+  );
+}
