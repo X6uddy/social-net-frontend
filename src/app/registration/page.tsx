c816e1d55de9a18ca414e4c5b3782d57a1b1bc29
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-19 21:00:33 +0400

    registation page created

---
+++ src/app/registration/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+
+import { NO_INDEX_PAGE } from '@/constants/seo.constans'
+import { Auth } from '@/components/screens/auth/Auth'
+
+export const metadata: Metadata = {
+    title: 'Registration',
+    ...NO_INDEX_PAGE
+}
+
+export default function RegistrationPage() {
+    return <Auth type='registration'/>
+}
