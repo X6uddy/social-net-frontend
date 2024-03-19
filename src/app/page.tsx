a58fd98196511c57f6283ee60d0a53ae1954a1af
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-19 20:59:14 +0400

    added chats page

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import { ChatsList } from "@/components/screens/chats/ChatList";
+import { CurrentUser } from "@/components/screens/chats/CurrentUser";
+import { Chat } from "@/components/screens/chats/Chat";
+
+export default function ChatsPage() {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: ".8fr 3fr",
+      }}
+    >
+      <div>
+        <CurrentUser />
+        <ChatsList />
+      </div>
+
+      <div>
+        <Chat />
+        <Chat />
+        <Chat />
+      </div>
+    </div>
+  );
+}
