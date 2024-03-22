2b25eb8157af3e4a065762c644d3699e7201cc6c
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-22 16:24:30 +0400

    added Tailwind styles

--- src/components/screens/auth/Auth.tsx
+++ src/components/screens/auth/Auth.tsx
@@ -1,20 +1,23 @@
 'use client'
+import { AtSign, KeyRound } from "lucide-react"
 
 import { Button } from "@/components/ui/button/Button"
 import Field from "@/components/ui/field/Field"
-import { AtSign, KeyRound } from "lucide-react"
+
+import styles from './Auth.module.scss';
 
 interface IAuth {
     type?: 'Login' | 'Registration'
 }
 
 export function Auth({type} : IAuth) {
-    return <div>
-        <form>
+    return <div className='flex w-screen h-full'>
+        <form className='m-auto block w-96 border border-rose-500 p-5'>
+            <h1 className="text-center">{type}</h1>
             <Field placeholder="Enter email" type="email" Icon={AtSign}/>
             <Field placeholder="Enter password" type="email" Icon={KeyRound}/>
-
-            <Button isLoading={false}>{type}</Button>
+            <br/>
+            <Button isLoading={true}>{type}</Button>
         </form>
     </div>
 }
