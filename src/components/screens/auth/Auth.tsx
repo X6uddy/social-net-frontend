cd968c6fa5914a1b863ad7f4f801ee73109c4f05
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-22 19:18:37 +0400

    changing the field's props

--- src/components/screens/auth/Auth.tsx
+++ src/components/screens/auth/Auth.tsx
@@ -2,22 +2,33 @@
 import { AtSign, KeyRound } from "lucide-react"
 
 import { Button } from "@/components/ui/button/Button"
-import Field from "@/components/ui/field/Field"
-
-import styles from './Auth.module.scss';
+import Field from "@/components/ui/field/Field";
 
 interface IAuth {
     type?: 'Login' | 'Registration'
 }
 
 export function Auth({type} : IAuth) {
-    return <div className='flex w-screen h-full'>
-        <form className='m-auto block w-96 border border-rose-500 p-5'>
-            <h1 className="text-center">{type}</h1>
-            <Field placeholder="Enter email" type="email" Icon={AtSign}/>
-            <Field placeholder="Enter password" type="email" Icon={KeyRound}/>
-            <br/>
-            <Button isLoading={true}>{type}</Button>
+    return <div className='flex h-full'>
+        <form className='m-auto block w-96 border border-[#8d3c3c57] p-8'>
+            <h1 className="text-center mb-7">{type}</h1>
+            <Field 
+                placeholder="Enter email" 
+                type="email" 
+                Icon={AtSign} 
+                className="mb-7"
+            />
+            <Field 
+                placeholder="Enter password" 
+                type="email" 
+                Icon={KeyRound}
+                className="mb-8"
+                error={{message: "Неверный пароль", type: 'min'}}
+            />
+            
+            <div className="text-center">
+                <Button isLoading={false}>{type}</Button>
+            </div>
         </form>
     </div>
 }
