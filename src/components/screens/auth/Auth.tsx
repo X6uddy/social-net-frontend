d933c408914fdbe0a8dd76a70b75f29c19e90d9c
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-24 18:40:43 +0400

    added onSubmit for authorization

--- src/components/screens/auth/Auth.tsx
+++ src/components/screens/auth/Auth.tsx
@@ -3,32 +3,61 @@ import { AtSign, KeyRound } from "lucide-react"
 
 import { Button } from "@/components/ui/button/Button"
 import Field from "@/components/ui/field/Field";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { IAuthFormState } from "./auth.types";
+import { signIn } from "next-auth/react";
 
 interface IAuth {
     type?: 'Login' | 'Registration'
 }
 
 export function Auth({type} : IAuth) {
-    return <div className='flex h-full'>
-        <form className='m-auto block w-96 border border-[#8d3c3c57] p-8'>
-            <h1 className="text-center mb-7">{type}</h1>
-            <Field 
-                placeholder="Enter email" 
-                type="email" 
-                Icon={AtSign} 
-                className="mb-7"
-            />
-            <Field 
-                placeholder="Enter password" 
-                type="email" 
-                Icon={KeyRound}
-                className="mb-8"
-                error={{message: "Неверный пароль", type: 'min'}}
-            />
-            
-            <div className="text-center">
-                <Button isLoading={false}>{type}</Button>
-            </div>
-        </form>
-    </div>
+    const {register, handleSubmit} = useForm<IAuthFormState>({
+        mode: 'onChange'
+    })
+
+    const onSubmit: SubmitHandler<IAuthFormState> = async (data) => {
+        if(type === 'Login') {
+            await signIn('credentials', {
+                redirect: false,
+                ...data
+            });
+        }
+    }
+
+    return (
+        <div className='flex h-full'>
+            <form onSubmit={handleSubmit(onSubmit)} className='m-auto block w-96 border border-[#8d3c3c57] p-8'>
+                <h1 className="text-center mb-7">{type}</h1>
+                <Field 
+                    {...register('email', {
+                        required: true
+                    })}
+                    placeholder="Enter email" 
+                    type="email" 
+                    Icon={AtSign} 
+                    className="mb-7"
+                />
+                <Field 
+                    {...register('password', {
+                        required: true
+                    })}
+                    placeholder="Enter password" 
+                    type="password" 
+                    Icon={KeyRound}
+                    className="mb-8"
+                    error={{message: "Неверный пароль", type: 'min'}}
+                />
+                
+                <div className="text-center">
+                    <Button 
+                        isLoading={false} 
+                        type="submit"
+                    >
+                    {type}
+                    </Button>
+                </div>
+            </form>
+        </div>
+    )
 }
