e83bf6ab8f0e6fbe1717ba8a62abf3428426df31
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-25 13:46:04 +0400

    added mainProvider

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,22 @@
+'use client'
 import { PropsWithChildren } from "react";
 import { Toaster } from "react-hot-toast";
 
 import { Sidebar } from "./sidebar/Sidebar";
 import styles from './Layout.module.scss';
+import MainProvider from "./MainProvider";
 
 export default function LayoutClient({children}:PropsWithChildren<unknown>) {
     return (
-        <main className={styles.layout}>
-            <Sidebar/>
-            <section>{children}</section>
-            <Toaster
-              position="top-center"
-              reverseOrder={false}
-            />
-        </main>
+        <MainProvider>
+            <main className={styles.layout}>
+                <Sidebar/>
+                <section>{children}</section>
+                <Toaster
+                    position="top-center"
+                    reverseOrder={false}
+                />
+            </main>
+        </MainProvider>
     )
 }
