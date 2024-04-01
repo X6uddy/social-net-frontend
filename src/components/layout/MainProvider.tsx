974c2575caa5ca2f215f6504768da47da3f471cf
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-01 10:31:11 +0400

    wrapped children in AuthProvider

--- src/components/layout/MainProvider.tsx
+++ src/components/layout/MainProvider.tsx
@@ -1,7 +1,13 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
-import type { PropsWithChildren } from 'react'
+import { type PropsWithChildren } from 'react'
+
+import AuthProvider from '@/app/providers/AuthProvider';
 
 export default function MainProvider({ children }: PropsWithChildren<unknown>) {
-    return <SessionProvider>{children}</SessionProvider>
+    return (
+        <SessionProvider>
+            <AuthProvider>{children}</AuthProvider>       
+        </SessionProvider>
+    )
 }
