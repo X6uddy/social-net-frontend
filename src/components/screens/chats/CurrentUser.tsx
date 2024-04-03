0a30628f5281763000166b4afc0af394cb66e827
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-03 10:29:58 +0400

    an avatar and a username have been added

--- src/components/screens/chats/CurrentUser.tsx
+++ src/components/screens/chats/CurrentUser.tsx
@@ -1,9 +1,25 @@
 'use client'
+import Image from "next/image";
+
 import { useAuth } from "@/hooks/useAuth";
+import no_profile from '/public/profile.png';
 
 export function CurrentUser() {
     const {user, isLoggedIn} = useAuth();
     return (
-        <div>{user?.username}</div>
+        <div className="p-2 flex items-center outline outline-1 outline-stone-600" >
+            <Image 
+                src={user?.avatar || no_profile} 
+                alt='user avatar' 
+                height={50}
+                width={70} 
+                className="mr-3"
+            />
+            <div className="flex flex-col">
+                <span>{user?.username}</span>
+                <span className=" text-sm opacity-30">Frontend_dev</span>
+            </div>
+            {/* <CircleEllipsis/> */}
+        </div>
     )
 }
