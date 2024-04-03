6001532cf546748d6455f18b34eefdb1961ca2ca
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-03 19:36:54 +0400

    added styles for the block with a list of chats and the current user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { ChatsList } from "@/components/screens/chats/chatList/ChatList";
+import { ChatsList } from "@/components/screens/chats/list/ChatList";
 import { CurrentUser } from "@/components/screens/chats/CurrentUser";
-import { Chat } from "@/components/screens/chats/Chat";
+import { Chat } from "@/components/screens/chats/chat/Chat";
 
 export default function ChatsPage() {
   return (
@@ -10,7 +10,7 @@ export default function ChatsPage() {
         gridTemplateColumns: ".8fr 3fr",
       }}
     >
-      <div>
+      <div className="flex flex-col border-r border-stone-600 border-opacity-50 divide-y divide-stone-600 divide-opacity-50">
         <CurrentUser />
         <ChatsList />
       </div>
