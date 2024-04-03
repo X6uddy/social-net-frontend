d2755223112db0ec5910029d4b12641c700183c9
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-03 19:39:43 +0400

    a component has been created to display information about the interlocutor

---
+++ src/components/screens/chats/list/ChatListItem.tsx
@@ -0,0 +1,28 @@
+import { User } from "lucide-react";
+import Image from "next/image";
+
+import no_profile from '/public/profile.png';
+
+interface IChatListItem {
+    
+}
+
+export function ChatListItem() {
+    return (
+        <div className="p-5 border-b border-stone-600 border-opacity-50 text-sm flex items-center">
+            <div>
+                <Image 
+                    src={no_profile} 
+                    alt='ChatUser avatar'
+                    width={50}
+                    height={70}
+                    className="mr-3 rounded-full border border-black border-opacity-30 bg-white"
+                />
+            </div>
+            <div className="flex flex-col">
+                <span>Мама</span>
+                <span className="text-sm opacity-30">Чтобы к 20:00 был дома!</span>
+            </div>
+        </div>
+    )
+}
