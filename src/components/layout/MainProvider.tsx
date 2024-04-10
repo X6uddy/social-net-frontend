67358a30d57846748955a8a925e0f11c73e15546
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-10 17:41:32 +0400

    added QueryClientProvider

--- src/components/layout/MainProvider.tsx
+++ src/components/layout/MainProvider.tsx
@@ -3,11 +3,15 @@ import { SessionProvider } from 'next-auth/react'
 import { type PropsWithChildren } from 'react'
 
 import AuthProvider from '@/app/providers/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function MainProvider({ children }: PropsWithChildren<unknown>) {
+    const queryClient = new QueryClient();
     return (
         <SessionProvider>
-            <AuthProvider>{children}</AuthProvider>       
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>{children}</AuthProvider>     
+            </QueryClientProvider>     
         </SessionProvider>
     )
 }
