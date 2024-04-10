dac68e27aac58cc3de5f463cbbbd222a51bb4da7
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-10 17:03:22 +0400

    added the display of the penPal and the last message

--- src/components/screens/chats/list/ChatListItem.tsx
+++ src/components/screens/chats/list/ChatListItem.tsx
@@ -1,28 +1,38 @@
+'use client'
 import { User } from "lucide-react";
 import Image from "next/image";
 
 import no_profile from '/public/profile.png';
 import { IChat } from "@/types/chat.types";
+import { useAuth } from "@/hooks/useAuth";
 
 interface IChatListItem {
-    
+    chat: IChat;
 }
 
-export function ChatListItem({}: IChat) {
+export function ChatListItem({chat}: IChatListItem) {
+    const {user, isLoggedIn} = useAuth();
+
+    const penPal = chat?.participants.find(people => people.email !== user?.email);
+    const lastMessage = chat?.messages.at(-1);
+
     return (
         <div className="p-5 border-b border-stone-600 border-opacity-50 text-sm flex items-center">
             <div>
                 <Image 
-                    src={no_profile} 
+                    src={penPal?.avatar?.url || no_profile} 
                     alt='ChatUser avatar'
                     width={50}
                     height={70}
-                    className="mr-3 rounded-full border border-black border-opacity-30 bg-white"
+                    className="rounded-full border border-black border-opacity-30 bg-white"
                 />
             </div>
-            <div className="flex flex-col">
-                <span>Мама</span>
-                <span className="text-sm opacity-30">Чтобы к 20:00 был дома!</span>
+            <div className="flex justify-between w-[100%]">
+                <div className="flex flex-col ml-3">
+                    <span>{penPal?.username || 'Мама'}</span>
+                    <span className="text-sm opacity-30">{lastMessage?.text || 'быстро домой'}</span>
+                </div>
+                <span className="text-sm opacity-30">{lastMessage?.createdAt || '12:30'}</span>
             </div>
         </div>
     )
