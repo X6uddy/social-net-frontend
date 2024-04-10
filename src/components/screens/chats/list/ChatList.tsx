4a14b0e3cb5ea3de30203a61c9e246d91c89f386
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-10 17:42:20 +0400

    added a request to get a list of chats

--- src/components/screens/chats/list/ChatList.tsx
+++ src/components/screens/chats/list/ChatList.tsx
@@ -1,22 +1,37 @@
+'use client'
 import { Search } from "lucide-react";
 
 import Field from "@/components/ui/field/Field";
 import { ChatListItem } from "./ChatListItem";
+import { useAuth } from "@/hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import { $fetch } from "@/$api/api.fetch";
+import { IChat } from "@/types/chat.types";
+import { Loader } from "@/components/ui/loader/Loader";
 
 export function ChatsList() {
+    const {data, isLoading} = useQuery({
+        queryKey: ['chats'],
+        queryFn: () => $fetch.get<IChat[]>('/chats')
+    })
     return (
-        // <div className="divide-y divide-stone-600 divide-opacity-50">
         <div>
             <div className=" border-b border-stone-600 border-opacity-50 p-5">
                 <Field
                     placeholder='Search chats'
                     Icon={Search}
-                    // className="px-5 pb-5"
                 />
             </div>
-            <ChatListItem/>
-            <ChatListItem/>
-            <ChatListItem/>
+            <div>
+                {isLoading ?
+                    <div className="p-[48%] text-center">
+                        <Loader/>
+                    </div>
+                : data?.length ? data.map(chat => 
+                    <ChatListItem key={chat.id} chat={chat} />) : 
+                    <p className="p-4">Chats not founded</p>
+                }
+            </div>
         </div>
     )
 }
