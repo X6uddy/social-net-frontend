4e2dc313388d62a2943274e569bf38efe3437f34
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-11 11:50:53 +0400

    added request to getting chats

--- src/components/screens/chats/list/ChatList.tsx
+++ src/components/screens/chats/list/ChatList.tsx
@@ -1,10 +1,9 @@
 'use client'
 import { Search } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
 
 import Field from "@/components/ui/field/Field";
 import { ChatListItem } from "./ChatListItem";
-import { useAuth } from "@/hooks/useAuth";
-import { useQuery } from "@tanstack/react-query";
 import { $fetch } from "@/$api/api.fetch";
 import { IChat } from "@/types/chat.types";
 import { Loader } from "@/components/ui/loader/Loader";
@@ -12,7 +11,7 @@ import { Loader } from "@/components/ui/loader/Loader";
 export function ChatsList() {
     const {data, isLoading} = useQuery({
         queryKey: ['chats'],
-        queryFn: () => $fetch.get<IChat[]>('/chats')
+        queryFn: () => $fetch.get<IChat[]>('/chats?populate=*')
     })
     return (
         <div>
@@ -27,7 +26,8 @@ export function ChatsList() {
                     <div className="p-[48%] text-center">
                         <Loader/>
                     </div>
-                : data?.length ? data.map(chat => 
+                : 
+                data?.length ? data.map(chat => 
                     <ChatListItem key={chat.id} chat={chat} />) : 
                     <p className="p-4">Chats not founded</p>
                 }
