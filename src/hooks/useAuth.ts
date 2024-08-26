4c70163f0a4d383ee4a543b749a0aa5103e7f1f6
X6uddy <sashavolkov_03@mail.ru>
2024-08-26 17:26:27 +0400

    adding isLoggedIn to return

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,12 +2,9 @@ import { useSession } from "next-auth/react";
 
 export function useAuth() {
     const {data, status, } = useSession();
-    console.log('use auth', data, 'status:', status)
     
     return {
         user: data?.user,
-        isLoggedIn: true,
-        status
-        // isLoggedIn: status === 'authenticated' // TODO вытаскивать из сессии статус  и обрабатывать в провайдере
+        isLoggedIn: status === 'authenticated' // TODO вытаскивать из сессии статус  и обрабатывать в провайдере
     }
 }
