dc3f26fe945bb1b0f58839bcf8bacbca8be772dc
X6uddy <sashavolkov_03@mail.ru>
2024-09-07 13:51:38 +0400

    adding chat search by username

--- src/components/screens/chats/list/ChatList.tsx
+++ src/components/screens/chats/list/ChatList.tsx
@@ -1,40 +1,58 @@
-'use client'
+"use client";
 import { Search } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 import Field from "@/components/ui/field/Field";
 import { ChatListItem } from "./ChatListItem";
 import { $fetch } from "@/$api/api.fetch";
 import { IChat } from "@/types/chat.types";
 import { Loader } from "@/components/ui/loader/Loader";
+import { useAuth } from "@/hooks/useAuth";
+import { useDebounce } from "@/hooks/useDebounce";
+
+//TODO сделать отработку ошибки если чатов нет
 
 export function ChatsList() {
-    const {data, isLoading} = useQuery({
-        queryKey: ['chats'],
-        queryFn: () => $fetch.get<{ data: IChat[] }>('/chats/1?populate=*', true)
-    })
-    console.log(data)
-    return (
-        <div>
-            <div className=" border-b border-stone-600 border-opacity-50 p-5">
-                <Field
-                    placeholder='Search chats'
-                    Icon={Search}
-                />
-            </div>
-            <div>
-                {isLoading ?
-                    <div className="p-[48%] text-center">
-                        <Loader/>
-                    </div>
-                : 
-                data?.length ? data.map(chat => 
-                    <ChatListItem key={chat.id} chat={chat} />) : 
-                    <ChatListItem key={1} chat={{id: '1', messages: [], participants: []}} />
-                    // <ChatListItem key={chat.id} chat={chat} />) : 
-                    // <p className="p-4">Chats not founded</p>
-                }
-            </div>
+  const { user, isLoggedIn } = useAuth();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const debouncedSearchTerm = useDebounce(searchTerm);
+
+  const { data, isLoading, isFetching, isError} = useQuery({
+    queryKey: ["chats", debouncedSearchTerm],
+    queryFn: () =>
+      $fetch.get<{ data: IChat[] }>(
+        // TODO &filters[$or][1][messages][text][$contains]=${debouncedSearchTerm} по сообщениям
+        `/chats?sort=createdAt:desc&populate[messages]=*&populate[participants][populate][avatar]=*&filters[participants][email][$eq]=${user?.email}&filters[$or][0][participants][username][$contains]=${debouncedSearchTerm}`,
+        true
+      ),
+    enabled: isLoggedIn,
+  });
+  console.log(data);
+
+  return (
+    <div>
+        <div className=" border-b border-stone-600 border-opacity-50 p-5">
+            <Field
+            placeholder="Search chats"
+            Icon={Search}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
         </div>
-    )
+        <div>
+            {isLoading || isFetching ? (
+                <div className='p-layout'>
+                    <Loader />
+                </div>
+			) : 
+            (
+                data?.data.map(chat => {
+                    return <ChatListItem key={chat.id} chat={chat} />
+                })
+            )}
+        </div>
+    </div>
+  );
 }
