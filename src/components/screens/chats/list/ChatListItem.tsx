e6f593b10f52151d6561e6a98f6c0569df8d8188
X6uddy <sashavolkov_03@mail.ru>
2024-09-07 13:52:24 +0400

    getting the avatar url and displaying the chat data

--- src/components/screens/chats/list/ChatListItem.tsx
+++ src/components/screens/chats/list/ChatListItem.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { User } from "lucide-react";
 import Image from "next/image";
+import dayjs from "dayjs";
 
 import no_profile from '/public/profile.png';
 import { IChat } from "@/types/chat.types";
 import { useAuth } from "@/hooks/useAuth";
+import { getImageUrl } from "@/app/config/get-image-url.config";
 
 interface IChatListItem {
     chat: IChat;
@@ -20,8 +22,8 @@ export function ChatListItem({chat}: IChatListItem) {
         <div className="p-5 border-b border-stone-600 border-opacity-50 text-sm flex items-center">
             <div>
                 <Image 
-                    src={penPal?.avatar?.url || no_profile} 
-                    alt='ChatUser avatar'
+                    src={getImageUrl(penPal?.avatar?.url) || no_profile} 
+                    alt={penPal?.email || ''}
                     width={50}
                     height={70}
                     className="rounded-full border border-black border-opacity-30 bg-white"
@@ -29,12 +31,12 @@ export function ChatListItem({chat}: IChatListItem) {
             </div>
             <div className="flex justify-between w-[100%]">
                 <div className="flex flex-col ml-3">
-                    <span>{penPal?.username || 'Мама'}</span>
+                    <span>{penPal?.username}</span>
                     <span className="text-sm opacity-30 w-[240px] whitespace-nowrap overflow-hidden text-ellipsis">
-                        {lastMessage?.text || 'Text Text Text Text Text Text Text Text Text Text'}
+                        {lastMessage?.text}
                     </span>
                 </div>
-                <span className="text-sm opacity-30">{lastMessage?.createdAt || '12:30'}</span>
+                <span className="text-sm opacity-30">{dayjs(lastMessage?.createdAt).format('HH:mm')}</span>
             </div>
         </div>
     )
